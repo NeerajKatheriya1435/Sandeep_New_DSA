1da40ec2c9efff192cb06a42ff535fd38de8e50e
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-08-11 20:54:31 +0530

    pattern printing

---
+++ day7.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 6. Check palindrome number
+    // int number=7887;
+    // int num=number;
+    // int rev=0;
+
+    // while (number>0)
+    // {
+    //     int ld=number%10;
+    //     rev=(rev*10)+ld;
+    //     number/=10;
+    // }
+    // // cout<<"Simple is: "<<num<<endl;
+    // // cout<<"Reverse is: "<<rev<<endl;
+
+    // if(rev==num){
+    //     cout<<"Number is: Palindrome"<<endl;
+    // }else{
+    //     cout<<"Number is not Palindrom"<<endl;
+    // }
+    
+
+    // 676
+    // 678
+
+    // 7. Count digits in a number.
+    // int number=7887;
+
+    // int counter=0;
+
+    // while (number>0)
+    // {
+    //     number/=10;
+    //     counter++;
+    // }
+
+    // cout<<"Total Digit is: "<<counter<<endl;
+    
+
+    /*
+
+    *
+    **
+    ***
+    ****
+    *****
+
+    A
+    AB
+    ABC
+    ABCD
+    ABCDE
+    */
+
+    // int num=5;
+    // char myChar='A';
+    // for (int i = 0; i <num; i++)
+    // {
+    //     for (int j = 0; j<=i; j++)
+    //     {
+    //         cout<<char(myChar+j);
+    //     }
+    //     cout<<endl;
+    // }
+
+    /*
+
+    *
+   ***
+  *****
+ *******
+*********
+
+    */
+    int num=5;
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= num-i; j++)
+        {
+            cout<<" ";
+        }
+        for (int k = 0; k < (2*i)-1; k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
+
+/*
+1gb //
+
+1gb and 20mb //
+
+1gb and 20+ 40mb //
+
+1gb+20mb+40mb
+*/
