92450a355b7ee3b667f3f38e65f01d861e174e6a
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-08-13 19:17:31 +0530

    Strcutre union and pointrs

---
+++ day9.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+// union Student {
+//         int rollNo;
+//         char name;
+//         float marks;
+// };
+
+// Function prototype (declaration)
+// int add(int,int);
+
+int main()
+{
+    // cout<<add(5,7);
+    // declare five size array
+    // int arr[]={4,5,7,7,8};
+    // int arr[5]={4,6,7,87,89};
+    // int a=7;
+    // int *arr;
+    // arr=new int[5]; 
+    // // // arr[0]=78;
+    // // // arr[1]=88;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     int value;
+    //     cout<<"Enter the value at "<<i<<" position"<<endl;
+    //     cin>>value;
+    //     arr[i]=value;
+    // }
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // delete[] arr;
+    // int arr[5]={3,7,9,5,2};
+
+    // int* ptr=arr;
+    // cout<<arr[2]<<endl;
+
+    // cout<<*arr<<endl;
+    // cout<<*(arr+1)<<endl;
+
+    // cout<<ptr<<endl;
+    // cout<<arr<<endl;
+
+    // int size;
+    // cin>>size;
+    // int num[size];
+
+    // typedef struct Student {
+    //     int rollNo;
+    //     string name;
+    //     float marks;
+    // } std;
+
+    
+
+    // Student s1;
+
+    // s1.rollNo=101;
+    // s1.marks=89;
+    // cout<<s1.rollNo<<endl;
+    // cout<<s1.marks<<endl;
+
+    // s1.marks=56;
+    // s1.name="Rohan";
+
+    // cout<<s1.marks<<endl;
+    // cout<<s1.rollNo<<endl;
+
+    // enum Weekday { Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun };
+
+    // cout<<Mon<<endl;
+    // cout<<Fri<<endl;
+    // if(Fri>Mon){
+    //     cout<<"Friday Comes after monday";
+    // }
+
+    // long long int arr = 45675678;
+    // typedef long long int llint;
+
+    // llint a=78978687;
+    // std s1;
+
+
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
