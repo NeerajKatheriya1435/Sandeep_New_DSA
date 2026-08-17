a06861b2098fe002ae239dbd744b09cf71f71b13
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-08-17 20:37:34 +0530

    Array Finished

---
+++ day12.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 10. Sort array in ascending order
+
+    // int num=6;
+    // int arr[num]={4,6,8,7,6,3};
+
+    // for (int i = 0; i < num; i++)
+    // {
+    //     for (int j = 0; j < num-1; j++)
+    //     {
+    //         if(arr[j]>arr[j+1]){
+    //             swap(arr[j],arr[j+1]);
+    //             // int temp=arr[j];
+    //             // arr[j]=arr[j+1];
+    //             // arr[j+1]=temp;
+    //         }
+    //     }  
+    // }
+    // for (int i = 0; i < num; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // 12. Find second largest element.
+
+    // int arr[]={34,7,2,17,19};
+
+    // // int max=INT32_MIN,secondLargest=INT32_MIN;
+    // int max=0,secondLargest=0;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if(max<arr[i]){
+    //         secondLargest=max;
+    //         max=arr[i];
+    //     }
+    //     else if(secondLargest<max && secondLargest<arr[i]){
+    //         secondLargest=arr[i];
+    //     }
+    // }
+    // cout<<"Max value: "<<max<<endl;
+    // cout<<"Second Max value: "<<secondLargest<<endl;
+
+
+    // 13. Remove duplicate elements.
+
+    // int arr[8]={4,6,8,6,2,5,2,5};
+    // int uniqueArr[10];
+
+    // int arr1[]={4,6,7};
+    // int arr2[]={3,5,8};
+
+    // int arr[6];
+    // int ind=0;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     arr[ind]=arr1[i];
+    //     ind++;
+    // }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     arr[ind]=arr2[i];
+    //     ind++;
+    // }
+
+    // for (int i = 0; i < ind; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+    
+    
+    
+    return 0;
+}
