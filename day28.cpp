f2f165cda8f5e91c2f1cf10adb6e6f726dc2a83a
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-09-07 12:48:01 +0530

    private protected public

---
+++ day28.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+// class Human{
+
+//     private:
+//         int age=45;
+//         string name="Shiva";
+
+//     protected:
+//         string language="English";
+    
+//     public:
+//         int salary=78000;
+//         void getData(){
+//             cout<<"My name is: "<<name<<endl;
+//             cout<<"My age is: "<<age<<endl;
+//         }
+// };
+
+// class Teacher:public Human{
+
+//     public:
+//         void printData(){
+//             cout<<salary<<endl;
+//             cout<<language<<endl;
+//         }
+// };
+
+
+// class Teacher:private Human{
+//     public:
+//         void printData(){
+//             cout<<salary<<endl;
+//             cout<<language<<endl;
+//         }
+// };
+
+// class Teacher:protected Human{
+//     public:
+//         void printData(){
+//             cout<<salary<<endl;
+//             cout<<language<<endl;
+//         }
+// };
+
+int main()
+{
+    
+    // Human h1;
+    // cout<<h1.language<<endl;
+    // h1.name="Shiva";
+    // h1.age=56;
+    // h1.getData();
+
+    // cout<<h1.name<<endl;
+    // cout<<h1.age<<endl;
+    // cout<<h1.age<<endl;
+    // h1.getData();
+    // cout<<h1.salary<<endl;
+    
+    // Teacher t1;
+    // cout<<t1.salary<<endl;
+    // t1.printData();
+
+    return 0;
+}
